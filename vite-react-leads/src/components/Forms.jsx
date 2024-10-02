2c805ebd483752db44711e763b8d52ed2d098802
ljcaster8 <boamorte_castro@yahoo.com.br>
2024-10-01 21:55:39 -0300

    update

--- vite-react-leads/src/components/Forms.jsx
+++ vite-react-leads/src/components/Forms.jsx
@@ -3,7 +3,36 @@ import Input from "../common-components/Input";
 import "./Forms.css";
 
 class Form extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            numPhone:''
+        }
+    }
+
+    handleChange = (event) => {
+        const { id, value } = event.target;
+        this.setState({
+            [id]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onHandleSubmit(this.state);
+    }
+
     render() {
+        // const name = this.state.name;
+        // const email = this.state.email;
+        // const numPhone = this.state.numPhone;
+
+        const { name, email, numPhone } = this.state;
+
         return(
             <aside>
                 <h2>Insira novos leads</h2>
@@ -12,21 +41,21 @@ class Form extends React.Component {
                 </p>
 
                 <form>
-                        <Input 
-                           id="nome"
-                           label="Nome completo:"
-                           placeholder="Ex.: Joao Pereira" />
-                        
-                        <Input 
-                           id="email"
-                           label="E-mail: "
-                           placeholder="Ex.: user@email.com" />
-                        
-                        <Input 
-                           id="celular"
-                           label="Celular: "
-                           placeholder="Ex.: +55 XX 1111-1111" />
-                        <button>Cadastrar</button>
+                        <div>
+                            <label htmlFor="name">Nome</label>
+                            <input type="text" id="name" placeholder="Ex.: Joao Pereira" onChange={this.handleChange} value={name}/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <input type="text" id="email" placeholder="Ex.: user@email.com" onChange={this.handleChange} value={email}/>
+                        </div>
+
+                        <div>
+                            <label htmlFor="numPhone">Celular</label>
+                            <input type="text" id="numPhone" placeholder="Ex.: +55 11 1111-11111" onChange={this.handleChange} value={numPhone}/>
+                        </div>
+                        <button onClick={this.handleSubmit}>Cadastrar</button>
                 </form>
             </aside>
         );
